68b7afb3f9d21e95510767629d5bf8359dabccf2
Ygor Santos <ygor.santos@symphony.is>
2022-11-28 10:29:03 -0300

    Add dev token and merchant account

--- sample-app/src/main/java/com/joinforage/android/example/ui/complete/flow/tokens/model/TokensUIDefaultState.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/complete/flow/tokens/model/TokensUIDefaultState.kt
@@ -1,6 +1,17 @@
 package com.joinforage.android.example.ui.complete.flow.tokens.model
 
-data class TokensUIDefaultState(
-    val bearer: String = "IP0oRQBc16nFG5xsHP5ViyIdajyb7Z",
-    val merchantAccount: String = "8000009"
-)
+import com.joinforage.android.example.BuildConfig
+
+class TokensUIDefaultState(
+    val isLoading: Boolean = false
+) {
+    val bearer = when (BuildConfig.FLAVOR) {
+        "dev" -> "yzXZvJxXXF19L7bIRXDeISsMj7YLNK"
+        else -> "IP0oRQBc16nFG5xsHP5ViyIdajyb7Z"
+    }
+
+    val merchantAccount = when (BuildConfig.FLAVOR) {
+        "dev" -> "9876545"
+        else -> "8000009"
+    }
+}
