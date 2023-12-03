31900a83bd1555ab508cc7ad886ca8e42214e20c
dleis612 <danny@joinforage.com>
2023-12-03 16:42:30 -0500

    Fix empty state for PIN

--- forage-android/src/test/java/com/joinforage/forage/android/core/element/state/ElementStateManagerTest.kt
+++ forage-android/src/test/java/com/joinforage/forage/android/core/element/state/ElementStateManagerTest.kt
@@ -7,7 +7,22 @@ import org.junit.Test
 class ElementStateManagerTest {
 
     @Test
-    fun testGetState() {
+    fun testGetStatePIN() {
+        val manager = PinElementStateManager.forEmptyInput()
+        val state = manager.getState()
+        assertThat(state.isFocused).isEqualTo(INITIAL_PIN_ELEMENT_STATE.isFocused)
+        assertThat(state.isBlurred).isEqualTo(INITIAL_PIN_ELEMENT_STATE.isBlurred)
+        assertThat(state.isEmpty).isEqualTo(INITIAL_PIN_ELEMENT_STATE.isEmpty)
+        assertThat(state.isValid).isEqualTo(INITIAL_PIN_ELEMENT_STATE.isValid)
+        assertThat(state.isComplete).isEqualTo(INITIAL_PIN_ELEMENT_STATE.isComplete)
+        assertThat(state.validationError).isEqualTo(INITIAL_PIN_ELEMENT_STATE.validationError)
+        // TODO: Compiler is very confused about accessing the state.details for the PIN
+        // This test fails because of it
+        assertThat(state.details).isEqualTo(INITIAL_PIN_ELEMENT_STATE.details)
+    }
+
+    @Test
+    fun testGetStatePAN() {
         val manager = PanElementStateManager.forEmptyInput()
         val state = manager.getState()
         assertThat(state.isFocused).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isFocused)
@@ -58,7 +73,7 @@ class ElementStateManagerTest {
     }
 
     @Test
-    fun testChangeFocus() {
+    fun testChangeFocusPIN() {
         val manager = PinElementStateManager.forEmptyInput()
         var state = manager.getState()
 
@@ -89,5 +104,39 @@ class ElementStateManagerTest {
         // cast to Any? to avoid ambiguous overload issue that
         // is unique to PinDetails because it's an alias for `Nothing?`
         assertThat(state.details as Any?).isEqualTo(INITIAL_PIN_ELEMENT_STATE.details)
+    }
+
+    @Test
+    fun testChangeFocusPAN() {
+        val manager = PanElementStateManager.forEmptyInput()
+        var state = manager.getState()
+
+        // focus
+
+        // only focus and blur should change
+        manager.changeFocus(true)
+        state = manager.getState()
+        assertThat(state.isFocused).isTrue
+        assertThat(state.isBlurred).isFalse
+        assertThat(state.isEmpty).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isEmpty)
+        assertThat(state.isValid).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isValid)
+        assertThat(state.isComplete).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isComplete)
+
+        // cast to Any? to avoid ambiguous overload issue that
+        // is unique to PinDetails because it's an alias for `Nothing?`
+        assertThat(state.details as Any?).isEqualTo(INITIAL_PAN_ELEMENT_STATE.details)
+
+        // unfocus
+        manager.changeFocus(false)
+        state = manager.getState()
+        assertThat(state.isFocused).isFalse
+        assertThat(state.isBlurred).isTrue
+        assertThat(state.isEmpty).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isEmpty)
+        assertThat(state.isValid).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isValid)
+        assertThat(state.isComplete).isEqualTo(INITIAL_PAN_ELEMENT_STATE.isComplete)
+
+        // cast to Any? to avoid ambiguous overload issue that
+        // is unique to PinDetails because it's an alias for `Nothing?`
+        assertThat(state.details as Any?).isEqualTo(INITIAL_PAN_ELEMENT_STATE.details)
     }
 }
