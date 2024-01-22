e57d8e068cc66202b0101ec632b64e4d02ddafc9
Evan Freeze <evan@joinforage.com>
2024-01-22 15:00:40 -0600

    add BalanceResultScreen

---
+++ sample-app/src/main/java/com/joinforage/android/example/ui/pos/screens/BalanceResultScreen.kt
@@ -0,0 +1,84 @@
+package com.joinforage.android.example.ui.pos.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.joinforage.android.example.ui.pos.data.BalanceCheck
+
+@Composable
+fun BalanceResultScreen(
+    balance: BalanceCheck?,
+    onBackButtonClicked: () -> Unit,
+    onDoneButtonClicked: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            if (balance == null) {
+                Text("There was a problem checking your balance.")
+            } else {
+                Text("View your EBT card balances")
+                Spacer(modifier = Modifier.height(16.dp))
+                Card {
+                    Column(
+                        modifier = Modifier.padding(16.dp).width(200.dp)
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text("EBT SNAP Balance: ")
+                            Text("$${balance.snap}")
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text("EBT Cash Balance: ")
+                            Text("$${balance.cash}")
+                        }
+                    }
+                }
+            }
+        }
+        if (balance == null) {
+            Button(onClick = onBackButtonClicked) {
+                Text("Try Again")
+            }
+        } else {
+            Button(onClick = onDoneButtonClicked) {
+                Text("Done")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BalanceResultScreenPreview() {
+    BalanceResultScreen(
+        balance = BalanceCheck(snap = "10.00", cash = "20.00"),
+        onBackButtonClicked = {},
+        onDoneButtonClicked = {}
+    )
+}
