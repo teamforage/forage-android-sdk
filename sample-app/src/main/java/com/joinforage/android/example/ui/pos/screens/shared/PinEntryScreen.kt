4bf53bbea748f354f346bd4078640073c6489171
Evan Freeze <evan@joinforage.com>
2024-01-25 16:29:33 -0600

    use ScreenWithBottomRow for PINEntryScreen

--- sample-app/src/main/java/com/joinforage/android/example/ui/pos/screens/shared/PinEntryScreen.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/pos/screens/shared/PinEntryScreen.kt
@@ -1,16 +1,12 @@
 package com.joinforage.android.example.ui.pos.screens.shared
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.joinforage.android.example.ui.pos.ui.ComposableForagePINEditText
 import com.joinforage.android.example.ui.pos.ui.ErrorText
+import com.joinforage.android.example.ui.pos.ui.ScreenWithBottomRow
 import com.joinforage.forage.android.ui.ForageConfig
 import com.joinforage.forage.android.ui.ForagePINEditText
 
@@ -23,14 +19,8 @@ fun PINEntryScreen(
     withPinElementReference: (element: ForagePINEditText) -> Unit,
     errorText: String? = null
 ) {
-    Column(
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    ScreenWithBottomRow(
+        mainContent = {
             if (paymentMethodRef != null) {
                 Text("Enter your card PIN")
                 ComposableForagePINEditText(
@@ -44,15 +34,17 @@ fun PINEntryScreen(
                 Text("There was an issue adding your card")
             }
             ErrorText(errorText)
-        }
-        Button(onClick = onBackButtonClicked) {
-            if (paymentMethodRef != null) {
-                Text("Back")
-            } else {
-                Text("Try Again")
+        },
+        bottomRowContent = {
+            Button(onClick = onBackButtonClicked) {
+                if (paymentMethodRef != null) {
+                    Text("Back")
+                } else {
+                    Text("Try Again")
+                }
             }
         }
-    }
+    )
 }
 
 @Preview
