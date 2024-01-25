e2ce3edd5293c23b69cb93bb09c6d2cbeb2e77b2
Evan Freeze <evan@joinforage.com>
2024-01-25 14:55:50 -0600

    use ScreenWithBottomRow for manual PAN entry screen

--- sample-app/src/main/java/com/joinforage/android/example/ui/pos/screens/shared/ManualPANEntryScreen.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/pos/screens/shared/ManualPANEntryScreen.kt
@@ -1,18 +1,15 @@
 package com.joinforage.android.example.ui.pos.screens.shared
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.joinforage.android.example.ui.pos.ui.ComposableForagePANEditText
+import com.joinforage.android.example.ui.pos.ui.ScreenWithBottomRow
 import com.joinforage.forage.android.ui.ForageConfig
 import com.joinforage.forage.android.ui.ForagePANEditText
 
@@ -23,14 +20,8 @@ fun ManualPANEntryScreen(
     onBackButtonClicked: () -> Unit,
     withPanElementReference: (element: ForagePANEditText) -> Unit
 ) {
-    Column(
-        verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    ScreenWithBottomRow(
+        mainContent = {
             Text("Manually enter your card number")
             Spacer(modifier = Modifier.height(8.dp))
             ComposableForagePANEditText(
@@ -42,11 +33,13 @@ fun ManualPANEntryScreen(
                 Text("Submit")
             }
             Spacer(modifier = Modifier.height(16.dp))
+        },
+        bottomRowContent = {
+            Button(onClick = onBackButtonClicked) {
+                Text("Back")
+            }
         }
-        Button(onClick = onBackButtonClicked) {
-            Text("Back")
-        }
-    }
+    )
 }
 
 @Preview
