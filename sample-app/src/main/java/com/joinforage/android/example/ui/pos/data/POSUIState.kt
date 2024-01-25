75808aecd2637c266e2fdc9a0874070be619f2a9
Devin Morgan <devin@joinforage.com>
2024-01-25 07:46:02 -0500

    Infer api baseUrl from sessionToken
    Previously the baseUrl was hardcoded to `dev`. Also extract a bit of
    common header-setting code and made a `PosApiService` factory method.

--- sample-app/src/main/java/com/joinforage/android/example/ui/pos/data/POSUIState.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/pos/data/POSUIState.kt
@@ -6,8 +6,8 @@ import com.joinforage.android.example.ui.pos.MerchantDetailsState
 import com.joinforage.forage.android.ui.ForageConfig
 
 data class POSUIState(
-    val merchantId: String = "",
-    val sessionToken: String = "<your_oath_or_session_token>", // <your_oath_or_session_token>,
+    val merchantId: String = "<your_merchant_id>", // <your_merchant_id>
+    val sessionToken: String = "<your_oath_or_session_token>", // <your_oath_or_session_token>
     val merchantDetailsState: MerchantDetailsState = MerchantDetailsState.Idle,
     val tokenizedPaymentMethod: PaymentMethod? = null,
     val balance: BalanceCheck? = null,
