0c979a984fb9fcac201cfa203475f320fd6b4ec6
Evan Freeze <evan@joinforage.com>
2024-01-25 16:22:17 -0600

    add error messages to POSUIState

--- sample-app/src/main/java/com/joinforage/android/example/ui/pos/data/POSUIState.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/pos/data/POSUIState.kt
@@ -9,11 +9,23 @@ data class POSUIState(
     val merchantId: String = "<your_merchant_id>", // <your_merchant_id>
     val sessionToken: String = "<your_oath_or_session_token>", // <your_oath_or_session_token>
     val merchantDetailsState: MerchantDetailsState = MerchantDetailsState.Idle,
+
+    // Tokenizing EBT Cards
     val tokenizedPaymentMethod: PaymentMethod? = null,
+    val tokenizationError: String? = null,
+
+    // Checking balances of those EBT Cards
     val balance: BalanceCheck? = null,
-    val localPayment: PosPaymentRequest? = null,
-    val createPaymentResponse: PaymentResponse? = null,
-    val capturePaymentResponse: PaymentResponse? = null
+    val balanceCheckError: String? = null,
+
+    // Creating a payment
+    val localPayment: PosPaymentRequest? = null, // Used to build up the payment object before we send it
+    val createdPayment: PaymentResponse? = null,
+    val paymentCreationError: String? = null,
+
+    // Capturing that payment
+    val capturedPayment: PaymentResponse? = null,
+    val paymentCaptureError: String? = null
 ) {
     val forageConfig: ForageConfig
         get() = ForageConfig(merchantId, sessionToken)
