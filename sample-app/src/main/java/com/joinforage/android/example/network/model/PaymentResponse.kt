5ebdccef8f41f4adaaec7e95da3379a99806383b
Devin Morgan <devin@joinforage.com>
2024-01-25 14:12:35 -0500

    Add posTerminal field to PaymentResponse schema
    We'll need to display some of this information on digital +
    physical receipts
    
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- sample-app/src/main/java/com/joinforage/android/example/network/model/PaymentResponse.kt
+++ sample-app/src/main/java/com/joinforage/android/example/network/model/PaymentResponse.kt
@@ -28,6 +28,21 @@ data class PaymentResponse(
     @Json(name = "success_date")
     var successDate: String?,
     var refunds: List<String>,
+    @Json(name = "pos_terminal")
+    var posTerminal: PosTerminal,
     @Json(name = "customer_id")
     var customerId: String?
+) {
+    val forageTerminalId
+        get() = posTerminal.terminalId
+    val deviceTerminalId
+        get() = posTerminal.providerTerminalId
+}
+
+@JsonClass(generateAdapter = true)
+data class PosTerminal(
+    @Json(name = "terminal_id")
+    var terminalId: String?,
+    @Json(name = "provider_terminal_id")
+    var providerTerminalId: String?
 )
