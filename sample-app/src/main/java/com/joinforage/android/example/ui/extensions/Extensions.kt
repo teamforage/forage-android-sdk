87693bc41b84d37953626eb293ba9f53df7cea8c
Danilo Joksimovic <danilo@joinforage.com>
2024-02-28 19:37:59 -0500

    Make things work with Appium

--- sample-app/src/main/java/com/joinforage/android/example/ui/extensions/Extensions.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/extensions/Extensions.kt
@@ -1,10 +1,13 @@
 package com.joinforage.android.example.ui.extensions
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
+import androidx.compose.ui.semantics.testTagsAsResourceId
 
+@OptIn(ExperimentalComposeUiApi::class)
 internal fun Modifier.withTestId(testId: String): Modifier = this.then(
-    semantics { testTag = testId; contentDescription = testId }
+    semantics { testTagsAsResourceId = true; testTag = "com.joinforage.android.example:id/$testId"; contentDescription = testId }
 )
