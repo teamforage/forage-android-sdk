796170ec7293b5f0945f7cb1a88b1157aae2f2f5
Evan Freeze <evan@joinforage.com>
2024-03-01 15:19:16 -0600

    simplify merchant data class to only the values we need for the UI

--- sample-app/src/main/java/com/joinforage/android/example/ui/pos/data/Merchant.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/pos/data/Merchant.kt
@@ -1,20 +1,13 @@
 package com.joinforage.android.example.ui.pos.data
 
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Merchant(
     val ref: String,
     val name: String,
-    @Json(name = "internal_name") val internalName: String,
     val fns: String,
-    val address: Address?,
-    val logo: String,
-    val homepage: String,
-    @Json(ignore = true, name = "third_party_api_keys") internal val thirdPartyApiKeys: Any? = null,
-    @Json(name = "possible_supported_benefits") val possibleSupportedBenefits: List<String>,
-    @Json(name = "uses_shopify") val usesShopify: Boolean
+    val address: Address?
 )
 
 @JsonClass(generateAdapter = true)
