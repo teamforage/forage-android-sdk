dd977b35b643b0933ef74bf8fd11f8ad8165cfef
Devin Morgan <devin@joinforage.com>
2024-03-14 12:27:10 -0400

    Modify interfaces to use ISO 4 PinBlocks
    Part of the ISO 4 spec for PIN blocks is to use aesEncryptEcb twice
    in the process of deriving the PIN block. So the method name
    `encryptPinBlock` was no longer appropriate. Since
    AndroidKeyStoreStoredKey already implemented the aesEncryptEcb
    method, for simplicity we opted to promote its method signature
    to be the standard interface signature for aesEncryptEcb

--- forage-android/src/main/java/com/joinforage/forage/android/pos/encryption/dukpt/WorkingKey.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/pos/encryption/dukpt/WorkingKey.kt
@@ -1,8 +1,7 @@
 package com.joinforage.forage.android.pos.encryption.dukpt
 
 import com.joinforage.forage.android.pos.encryption.AesBlock
-import com.joinforage.forage.android.pos.encryption.PinBlock
 
 internal interface WorkingKey {
-    fun encryptPinBlock(pinBlock: PinBlock): AesBlock
+    fun aesEncryptEcb(aesBlock: AesBlock): AesBlock
 }
