70b017cadaedfc9f8cd1d7d7c0cbbfc4b1973022
Devin Morgan <devin@joinforage.com>
2024-06-26 09:15:48 -0400

    Delete inappropriate files
    In our attempt to bring the changes from
    `devin/all-the-things-for-forage-pin-pad` into `main`, resolving
    changes on that branch with changes from `upstream/main` is revealing a
    number of files that should no longer exist in the Pos `main` branch.
    All of these files have either been moved or ought to be deleted.

--- forage-android/src/main/java/com/joinforage/forage/android/pos/encryption/dukpt/WorkingKey.kt
+++
@@ -1,7 +0,0 @@
-package com.joinforage.forage.android.pos.encryption.dukpt
-
-import com.joinforage.forage.android.pos.encryption.AesBlock
-
-internal interface WorkingKey {
-    fun aesEncryptEcb(aesBlock: AesBlock): AesBlock
-}
