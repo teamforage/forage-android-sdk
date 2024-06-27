e328fc44c67ecf12de963d5e0b34ddffdf110377
Devin Morgan <devin@joinforage.com>
2024-06-27 09:07:23 -0400

    Modify catalog view to recreate issue
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- sample-app/src/main/java/com/joinforage/android/example/ui/catalog/CatalogFragment.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/catalog/CatalogFragment.kt
@@ -1,22 +1,20 @@
 package com.joinforage.android.example.ui.catalog
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.joinforage.android.example.R
 import com.joinforage.android.example.databinding.FragmentCatalogBinding
 import com.joinforage.forage.android.core.services.ForageConfig
-import com.joinforage.forage.android.ecom.ui.element.ForagePINEditText
 
 class CatalogFragment : Fragment() {
 
     private var _binding: FragmentCatalogBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,40 +22,65 @@ class CatalogFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val catalogViewModel = ViewModelProvider(this)[CatalogViewModel::class.java]
-
         _binding = FragmentCatalogBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // any string prefixed with "dev_" so that we can use
+        // the dev feature flags to test Rosetta vs BT
         val forageConfig = ForageConfig(
-            sessionToken = "sandbox_420FoEvaMon",
+            sessionToken = "dev_eyJhIjogNDI5ODYsICJzayI6ICJOd3haaHVmQnBybDlaU0VBb194SFNRPT0iLCAidCI6IDQ3fQ==.ZnyEXA.Msl6EOi1byNi27TfMB0fD3hT4tc",
             merchantId = "0123456"
         )
 
-        // ForagePANEditText can be created via XML layouts or
-        // dynamically. These different modes of creation are
-        // a natural point of styling differences to emerge.
-        // We add a ForagePANEditText to the catalog in the
-        // hopes that we'll spot these discrepancies
-        // NOTE: this view is currently unstyled compared to
-        // the other ForagePINEditText in the catalog which was
-        // created via XML and has some XML styles associated
-        // with it.
-        val dynamicPinEditText = ForagePINEditText(requireContext(), null, R.attr.catalogPinStyleRef)
-        binding.root.addView(dynamicPinEditText)
-
-        dynamicPinEditText.setForageConfig(forageConfig)
-        binding.firstForageEditText.setForageConfig(forageConfig)
-        binding.foragePinEditText.setForageConfig(forageConfig)
-
-        // NOTE: we call setForageConfig a second time here so that
-        //  the CI tests always confirm that running setForageConfig
-        //  more than once is OK and does not cause a crash. So,
-        //  these duplicate calls are intentional here
         binding.firstForageEditText.setForageConfig(forageConfig)
         binding.foragePinEditText.setForageConfig(forageConfig)
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // anybody can get a binding to a ForagePINEditTExt
+        val foragesSuperSafeEditTextElement = binding.foragePinEditText
+
+        // anybody can repeatedly explore the view hierarchy by running
+        // their app in debug mode and repeatedly doing `.getChildAt(...)`
+        // to observe child views
+        val isRosetta = (foragesSuperSafeEditTextElement.getChildAt(0) as ViewGroup).getChildAt(0) is EditText
+        if (isRosetta) {
+            println("Using Rosetta")
+            // once somebody understands the view hierarchy of Rosetta...
+            val editText = (foragesSuperSafeEditTextElement.getChildAt(0) as ViewGroup).getChildAt(0) as EditText
+
+            // ...they will have free rein to observe whatever a user
+            // in the PIN field
+            // NOTE: .addTextChangedListener is just one way to observe
+            // the text there other ways to do observe the PIN once you
+            // have a reference to the view
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, st: Int, co: Int, af: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) { println("Rosetta Pin Content: $s") }
+
+            })
+        } else {
+            println("Using Basis Theory")
+
+            // once somebody understands the view hierarchy of Basis Theory...
+            val appCompatEditText = ((foragesSuperSafeEditTextElement.getChildAt(0) as ViewGroup).getChildAt(0) as ViewGroup).getChildAt(0) as AppCompatEditText
+
+            // ...they will have free rein to observe whatever a user
+            // in the PIN field
+            // NOTE: .addTextChangedListener is just one way to observe
+            // the text there other ways to do observe the PIN once you
+            // have a reference to the view
+            appCompatEditText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, st: Int, co: Int, af: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) { println("BT Pin Content: $s") }
+
+            })
+        }
     }
 
     override fun onDestroyView() {
