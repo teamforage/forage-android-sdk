077ebb543251b8da93aefa9296ff0e9e977c103d
Devin Morgan <devin@joinforage.com>
2024-06-28 13:01:24 -0400

    Get rid of unnecessary files for demo
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- sample-app/src/main/java/com/joinforage/android/example/MainActivity.kt
+++ sample-app/src/main/java/com/joinforage/android/example/MainActivity.kt
@@ -22,26 +22,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_complete_flow,
-                R.id.navigation_catalog
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            onBackPressedDispatcher.onBackPressed()
-            return true
-        }
-        return true
-    }
 }
