f95caa4c7c7e4267e9023cef024a12c800f9de31
Devin Morgan <devin@joinforage.com>
2024-07-23 13:53:08 -0400

    Whitelist PAN prefix for POS pin encryption testing (#296)
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- forage-android/src/main/java/com/joinforage/forage/android/core/ui/element/state/pan/PanInputState.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/core/ui/element/state/pan/PanInputState.kt
@@ -37,7 +37,8 @@ internal class PanInputState(
                 PaymentCaptureErrorCard(),
                 BalanceCheckErrorCard(),
                 NonProdValidEbtCard(),
-                EmptyEbtCashBalanceCard()
+                EmptyEbtCashBalanceCard(),
+                PinEncryptionTestCard()
             )
         )
     }
