c2ffe5d93b10db079fa62b4833e08d62c1ce7fab
Devin Morgan <devin@joinforage.com>
2024-07-24 08:54:25 -0400

    Re-drop setting API-VERSION: default in AbstractVaultSubmitter
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- forage-android/src/main/java/com/joinforage/forage/android/core/services/vault/AbstractVaultSubmitter.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/core/services/vault/AbstractVaultSubmitter.kt
@@ -113,7 +113,6 @@ internal abstract class AbstractVaultSubmitter(
         .setHeader(ForageConstants.Headers.MERCHANT_ACCOUNT, params.merchantId)
         .setHeader(ForageConstants.Headers.IDEMPOTENCY_KEY, params.idempotencyKey)
         .setHeader(ForageConstants.Headers.TRACE_ID, logger.getTraceIdValue())
-        .setHeader(ForageConstants.Headers.API_VERSION, "default")
         .setHeader(ForageConstants.Headers.SESSION_TOKEN, "${ForageConstants.Headers.BEARER} ${params.sessionToken}")
         .setToken(vaultToken)
 
