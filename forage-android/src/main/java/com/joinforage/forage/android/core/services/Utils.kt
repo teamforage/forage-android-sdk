b8f3e8f2f14f3cacb35d4262ad31135d2566fea9
Devin Morgan <devin@joinforage.com>
2024-09-02 15:28:46 -0400

    Drop polling jitter
    We no longer poll for pin-submission actions because we use sync
    endpoints (backend polls). Let's delete this unnecessary code

--- forage-android/src/main/java/com/joinforage/forage/android/core/services/Utils.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/core/services/Utils.kt
@@ -2,18 +2,6 @@ package com.joinforage.forage.android.core.services
 
 import okhttp3.HttpUrl
 import org.json.JSONObject
-import kotlin.random.Random
-
-/**
- * We generate a random jitter amount to add to our retry delay when polling for the status of
- * Payments and Payment Methods so that we can avoid a thundering herd scenario in which there are
- * several requests retrying at the same exact time.
- *
- * Returns a random integer between -25 and 25
- */
-internal fun getJitterAmount(random: Random = Random.Default): Int {
-    return random.nextInt(-25, 26)
-}
 
 internal fun HttpUrl.Builder.addTrailingSlash(): HttpUrl.Builder {
     return this.addPathSegment("")
