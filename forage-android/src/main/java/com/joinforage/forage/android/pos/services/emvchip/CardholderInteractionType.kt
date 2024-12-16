538dd9f5b5940f13db9658346415f21cb68c95b6
Devin Morgan <devin@joinforage.com>
2024-12-16 08:21:53 -0500

    Prune emvchip service's responsibilities
    We decided that Payments API will have complete say of interpreting the
    track 2 data and deciding whether a card is legacy vs fallback. So, we
    should get rid of all Android client code that interprets the track 2
    data. Also, let's drop all the EMV and Field 55 related things until we
    cross that bridge.

--- forage-android/src/main/java/com/joinforage/forage/android/pos/services/emvchip/CardholderInteractionType.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/pos/services/emvchip/CardholderInteractionType.kt
@@ -2,9 +2,6 @@ package com.joinforage.forage.android.pos.services.emvchip
 
 enum class CardholderInteractionType(val value: String) {
     KeyEntry("manual"),
-    MagSwipeLegacy("magstripe"), 
-    MagSwipeFallback("magstripe_fallback"),
-    Insert("insert"),
-    Tap("tap"),
+    MagSwipe("magstripe"),
     Unknown("unknown")
 }
--- forage-android/src/main/java/com/joinforage/forage/android/pos/services/emvchip/TapEMVInteraction.kt
+++
@@ -1,7 +0,0 @@
-package com.joinforage.forage.android.pos.services.emvchip
-
-class TapEMVInteraction(track2Data: String, emvField55Data: String) : EMVInteraction(
-    track2Data,
-    emvField55Data,
-    CardholderInteractionType.Tap
-)
