638e4518f022d740da32c93b51254149cb74495f
Devin Morgan <devin@joinforage.com>
2025-02-07 18:44:25 -0800

    Delete pos code from Ecom repo
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- forage-android/src/main/java/com/joinforage/forage/android/pos/services/vault/submission/PinTranslationParams.kt
+++
@@ -1,7 +0,0 @@
-package com.joinforage.forage.android.pos.services.vault.submission
-
-internal data class PinTranslationParams(
-    val encryptedPinBlock: String,
-    val keySerialNumber: String,
-    val txnCounter: String
-)
