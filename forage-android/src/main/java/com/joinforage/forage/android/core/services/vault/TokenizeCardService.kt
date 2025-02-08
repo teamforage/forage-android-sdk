1fb32bb9c8a2fc2daa4db0e86c612d109ac3c67f
Devin Morgan <devin@joinforage.com>
2025-02-07 18:47:35 -0800

    Make TokenizeCardService use IHttpEngine
    Signed-off-by: Devin Morgan <devin@joinforage.com>

--- forage-android/src/main/java/com/joinforage/forage/android/core/services/vault/TokenizeCardService.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/core/services/vault/TokenizeCardService.kt
@@ -1,64 +1,66 @@
 package com.joinforage.forage.android.core.services.vault
 
-import com.joinforage.forage.android.core.services.ForageConstants
-import com.joinforage.forage.android.core.services.addTrailingSlash
+import com.joinforage.forage.android.core.services.ForageConfig
+import com.joinforage.forage.android.core.services.forageapi.engine.HttpRequestFailedException
+import com.joinforage.forage.android.core.services.forageapi.engine.IHttpEngine
 import com.joinforage.forage.android.core.services.forageapi.network.ForageApiResponse
-import com.joinforage.forage.android.core.services.forageapi.network.NetworkService
-import com.joinforage.forage.android.core.services.forageapi.paymentmethod.PaymentMethodRequestBody
-import com.joinforage.forage.android.core.services.forageapi.paymentmethod.RequestBody
-import com.joinforage.forage.android.core.services.telemetry.Log
-import okhttp3.HttpUrl
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.IOException
+import com.joinforage.forage.android.core.services.forageapi.network.UnknownErrorApiResponse
+import com.joinforage.forage.android.core.services.forageapi.network.UnknownTimeoutErrorResponse
+import com.joinforage.forage.android.core.services.forageapi.paymentmethod.PaymentMethodService
+import com.joinforage.forage.android.core.services.telemetry.LogLogger
+import com.joinforage.forage.android.core.services.vault.errors.IErrorStrategy
 
 internal class TokenizeCardService(
-    private val httpUrl: String,
-    okHttpClient: OkHttpClient,
-    private val logger: Log
-) : NetworkService(okHttpClient, logger) {
-    suspend fun tokenizeCard(cardNumber: String, customerId: String? = null, reusable: Boolean = true): ForageApiResponse<String> = try {
-        logger.i(
-            "[HTTP] POST request for Payment Method",
-            attributes = mapOf(
-                "customer_id" to customerId
-            )
+    val logger: LogLogger,
+    val forageConfig: ForageConfig,
+    val pmService: PaymentMethodService,
+) {
+
+    private suspend fun _tokenizeCard(
+        cardNumber: String,
+        customerId: String?,
+        reusable: Boolean
+    ): ForageApiResponse<String> {
+        val response = pmService.createPaymentMethod(
+            rawPan = cardNumber,
+            customerId = customerId,
+            reusable = reusable
         )
-        tokenizeCardCoroutine(
-            PaymentMethodRequestBody(
-                cardNumber = cardNumber,
-                customerId = customerId,
-                reusable = reusable
-            )
-        )
-    } catch (ex: IOException) {
-        logger.e("[HTTP] Failed while tokenizing PaymentMethod", ex, attributes = mapOf("customer_id" to customerId))
-        ForageApiResponse.Failure(500, "unknown_server_error", ex.message.orEmpty())
+        return ForageApiResponse.Success(response.json)
     }
 
-    private suspend fun tokenizeCardCoroutine(requestBody: RequestBody): ForageApiResponse<String> {
-        val url = getTokenizeCardUrl()
-        val okHttpRequestBody = requestBody
-            .toJSONObject()
-            .toString()
-            .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
-
-        val request: Request = Request.Builder()
-            .url(url)
-            .header(ForageConstants.Headers.API_VERSION, "2023-05-15")
-            .post(okHttpRequestBody)
-            .build()
-
-        return convertCallbackToCoroutine(request)
+    suspend fun tokenizeCard(
+        cardNumber: String,
+        customerId: String?,
+        reusable: Boolean
+    ): ForageApiResponse<String> = try {
+        _tokenizeCard(cardNumber, customerId, reusable)
+    } catch (e: Throwable) {
+        TokenizationErrorStrategy(logger).handleError(e) { /* Do Nothing ... */ }
     }
+}
 
-    private fun getTokenizeCardUrl(): HttpUrl = httpUrl.toHttpUrlOrNull()!!
-        .newBuilder()
-        .addPathSegment(ForageConstants.PathSegment.API)
-        .addPathSegment(ForageConstants.PathSegment.PAYMENT_METHODS)
-        .addTrailingSlash()
-        .build()
+internal class TokenizationErrorStrategy(private val logLogger: LogLogger) : IErrorStrategy {
+    override suspend fun handleError(error: Throwable, cleanup: () -> Unit): ForageApiResponse<String> {
+        return when (error) {
+            is PaymentMethodService.FailedToCreatePaymentMethodException -> {
+                cleanup()
+                logLogger.e("[END] Failed to tokenize PaymentMethod.")
+                UnknownErrorApiResponse
+            }
+            is HttpRequestFailedException -> {
+                cleanup()
+                logLogger.e("[END] Failed to tokenize PaymentMethod.", error.cause)
+                when (error.cause) {
+                    is java.net.SocketTimeoutException -> UnknownTimeoutErrorResponse
+                    else -> UnknownErrorApiResponse
+                }
+            }
+            else -> {
+                cleanup()
+                logLogger.e("[END] Failed to tokenize PaymentMethod.", error)
+                UnknownErrorApiResponse
+            }
+        }
+    }
 }
