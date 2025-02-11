564ba23d23587b76391b89a33f0613163608b05f
Devin Morgan <devin@joinforage.com>
2025-02-11 08:25:55 -0500

    ForagePinElement to absorb VaultWrapper
    VaultWrapper was necessary when we had three different backing vaults.
    Now that we just use rosetta, it's legacy complexity.

--- forage-android/src/main/java/com/joinforage/forage/android/pos/ui/element/ForagePINEditText.kt
+++ forage-android/src/main/java/com/joinforage/forage/android/pos/ui/element/ForagePINEditText.kt
@@ -1,15 +1,9 @@
 package com.joinforage.forage.android.pos.ui.element
 
 import android.content.Context
-import android.graphics.Typeface
 import android.util.AttributeSet
 import com.joinforage.forage.android.R
-import com.joinforage.forage.android.core.services.EnvConfig
-import com.joinforage.forage.android.core.services.vault.RosettaPinSubmitter
-import com.joinforage.forage.android.core.ui.VaultWrapper
 import com.joinforage.forage.android.core.ui.element.ForagePinElement
-import com.joinforage.forage.android.core.ui.getLogoImageViewLayout
-import com.joinforage.forage.android.pos.ui.vault.rosetta.RosettaPinElement
 
 /**
  * A [ForagePinElement] that securely collects a card PIN. You can use a [ForagePINEditText]
@@ -47,33 +41,4 @@ class ForagePINEditText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.foragePanEditTextStyle
-) : ForagePinElement(context, attrs, defStyleAttr) {
-    override val vault: VaultWrapper
-
-    init {
-        context.obtainStyledAttributes(attrs, R.styleable.ForagePINEditText, defStyleAttr, 0)
-            .apply {
-                try {
-
-                    // initialize here since these params are available
-                    vault = RosettaPinElement(context, attrs, defStyleAttr)
-                } finally {
-                    recycle()
-                }
-            }
-
-        _linearLayout.addView(vault.getTextElement())
-        _linearLayout.addView(getLogoImageViewLayout(context))
-        addView(_linearLayout)
-    }
-
-    override var typeface: Typeface?
-        get() = vault.typeface
-        set(value) { vault.typeface = value }
-
-    override fun showKeyboard() = vault.showKeyboard()
-
-    override fun getVaultSubmitter(
-        envConfig: EnvConfig
-    ): RosettaPinSubmitter = vault.getVaultSubmitter(envConfig)
-}
+) : ForagePinElement(context, attrs, defStyleAttr)
