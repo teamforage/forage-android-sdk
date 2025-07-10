f170f9e144325be852f79df264cc413369da4f4c
Marc Parmet <marc@joinforage.com>
2025-07-09 22:20:28 -0400

    [EXPA-206] Better tolerate defective session tokens
    Using an access token or a defective session token (like
    dev_sessionToken1234) would crash the app.

--- sample-app/src/main/java/com/joinforage/android/example/ui/complete/flow/tokenize/FlowTokenizeViewModel.kt
+++ sample-app/src/main/java/com/joinforage/android/example/ui/complete/flow/tokenize/FlowTokenizeViewModel.kt
@@ -50,35 +50,39 @@ class FlowTokenizeViewModel @Inject constructor(
     fun onSubmit(foragePanEditText: ForagePANEditText) = viewModelScope.launch {
         _isLoading.value = true
 
-        val response = ForageSDK().tokenizeEBTCard(
-            TokenizeEBTCardParams(
-                foragePanEditText = foragePanEditText,
-                customerId = "android-test-customer-id-2"
+        try {
+            val response = ForageSDK().tokenizeEBTCard(
+                TokenizeEBTCardParams(
+                    foragePanEditText = foragePanEditText,
+                    customerId = "android-test-customer-id-2"
+                )
             )
-        )
 
-        when (response) {
-            is ForageApiResponse.Success -> {
-                Log.d(TAG, "Tokenize EBT card Response: ${response.data}")
-                val paymentMethod = response.toPaymentMethod()
-                // We include the `as` casting and the `when` block below
-                // to ensure that the models are exported as desired
-                // And support the desired casting + language features
-                Log.d(TAG, "EBT Card: ${paymentMethod.card.last4}")
+            when (response) {
+                is ForageApiResponse.Success -> {
+                    Log.d(TAG, "Tokenize EBT card Response: ${response.data}")
+                    val paymentMethod = response.toPaymentMethod()
+                    // We include the `as` casting and the `when` block below
+                    // to ensure that the models are exported as desired
+                    // And support the desired casting + language features
+                    Log.d(TAG, "EBT Card: ${paymentMethod.card.last4}")
 
-                val ebtCard = paymentMethod.card as EbtCard
-                Log.d(TAG, "EBT Card: ${ebtCard.last4} ${ebtCard.usState}")
-                when (val card: Card = paymentMethod.card) {
-                    is EbtCard -> {
-                        assert(card.last4.length == 4)
-                        Log.d(TAG, "EBT Card: ${card.last4} ${card.usState}")
+                    val ebtCard = paymentMethod.card as EbtCard
+                    Log.d(TAG, "EBT Card: ${ebtCard.last4} ${ebtCard.usState}")
+                    when (val card: Card = paymentMethod.card) {
+                        is EbtCard -> {
+                            assert(card.last4.length == 4)
+                            Log.d(TAG, "EBT Card: ${card.last4} ${card.usState}")
+                        }
                     }
+                    _paymentMethod.value = paymentMethod
                 }
-                _paymentMethod.value = paymentMethod
+                is ForageApiResponse.Failure -> {
+                    _error.value = response.error.message
+                }
             }
-            is ForageApiResponse.Failure -> {
-                _error.value = response.error.message
-            }
+        } catch (e: Exception) {
+            _error.value = e.toString()
         }
 
         _isLoading.value = false
